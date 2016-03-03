ee9fa065cbb8b6a6cf06334b41da86b2f2e8fd60
P Evans <evanspc@gmail.com>
2016-03-02 17:15:43 -0800

    Removed folder hw5and6

--- src/cp120a/evanspc/hw3/Shapes/Triangle.java
+++
@@ -1,6 +0,0 @@
-package cp120a.evanspc.hw3;
-
-public class Triangle {
-// wasn't able to get to this, sorry
-	//intended to implement via three sets of points, checking that they weren't in a line
-}
